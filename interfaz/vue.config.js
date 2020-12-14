3522d7e99d06c0150dca97188d5ef497aed2813f
JOS3 <josechocojx@gmail.com>
2020-12-14 12:52:13 -0600

    Manejo de versiones. Versión de producción

--- interfaz/vue.config.js
+++ interfaz/vue.config.js
@@ -1,3 +1,4 @@
+const version = '0.001'
 module.exports = {
 	filenameHashing: false,
 	publicPath: process.env.NODE_ENV === 'production' ? '/increscendo/assets' : '/',
@@ -5,5 +6,10 @@ module.exports = {
     indexPath: '../index.html',
 	devServer: {
 		proxy: 'http://localhost/increscendo_app'
-	}
+	},
+  	chainWebpack: config => {
+  		config.output
+  		.filename(`js/[name].[hash].${version}.js`)
+  		.chunkFilename(`js/[name].[hash].${version}.js`)
+  	}
 }
