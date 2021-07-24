94ca9b1c50a0ee9198861c5f8bcba2f1d843f224
danytujal <danytujal@gmail.com>
2021-07-24 12:35:24 -0600

    Slide

--- interfaz/src/views/Landing.vue
+++ interfaz/src/views/Landing.vue
@@ -77,6 +77,23 @@
 				<div class="contenedor-slider">
 					<div class="content-img-slider">
 						<img id="bg-home" src="../assets/img/bgslide.jpg" height="100vh" alt="slider-img">
+
+					
+<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="..." alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="..." alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="..." alt="Third slide">
+    </div>
+  </div>
+</div>
+
+
 					</div>
 					<div class="contenedor-sec-slider">
 						<div class="card-actions-slider">
