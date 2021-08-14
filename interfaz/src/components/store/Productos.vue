034c7d4c79dc3323ec54c1ef2c622b6f3187f366
danytujal <danytujal@gmail.com>
2021-08-13 22:20:54 -0600

    html de cards

--- interfaz/src/components/store/Productos.vue
+++ interfaz/src/components/store/Productos.vue
@@ -1,23 +1,52 @@
 <template>
     <div>
+        <!-- Load -->
         <div class="col-12 text-center" v-if="buscando">
             <div class="spinner-border" role="status"></div>
         </div>
+        <!-- Mensaje "sin existencias" -->
         <div class="col-12 text-center" v-if="lista.length == 0">
             <h2 class="text-muted">Aún no tenemos productos, pero pronto actualizaremos la tienda</h2>
         </div>
-        <div class="col-12" v-if="actual == 1 && !buscando">
-            <div class="card col-3" v-for="(i, key) in lista" :key="key">
+        <!-- Tarjeta de producto -->
+        <!-- <div class="col-2" v-if="actual == 1 && !buscando">
+            <div class="" v-for="(i, key) in lista" :key="key">
                 <a href="javascript:;" @click="detalleProducto(i)">
                     <img :src="i.imagen_link" class="card-img-top">
-                    <div class="card-body">
+                    <div class="">
                         <h4>{{i.nombre}}</h4>
                         <h6>Q {{i.precio_venta}}</h6>
                     </div>
                 </a>
             </div>
-        </div>
+        </div> -->
 
+
+    <div class="gallery_store" v-if="actual == 1 && !buscando">
+        <!--  -->
+        <div class="content_categoria" v-for="(i, key) in lista" :key="key">
+            <img class="product_img_categoria" :src="i.imagen_link">
+            <h3 class="categoria_h3">{{i.nombre}}</h3>
+            <p class="p_store_categoria">Descripción del producto</p>
+            <h6 class="price_categoria">Q {{i.precio_venta}}</h6>
+            <ul class="stars_categoria">
+            <li><i class="fa fa-star" aria-hidden="true"></i></li>
+            <li><i class="fa fa-star" aria-hidden="true"></i></li>
+            <li><i class="fa fa-star" aria-hidden="true"></i></li>
+            <li><i class="fa fa-star" aria-hidden="true"></i></li>
+            <li><i class="fa fa-star" aria-hidden="true"></i></li>
+            </ul>
+            <a href="javascript:;" @click="detalleProducto(i)">
+                <button class="buy-1 btn_cards_categoria">Comprar</button>
+            </a>
+        </div>
+    </div>
+
+
+
+
+
+        <!-- Detalle del producto -->
         <div class="card" v-if="actual == 2">
             <div class="card-header">
                 <div class="col-12 form-row">
@@ -33,10 +62,12 @@
                 <img :src="producto.imagen_link" class="img-thumbnail avatar m-0 p-0">
             </div>
         </div>
+        <!-- fin -->
     </div>
 </template>
 <script>
-import Formulario from "@/mixins/Formulario.js";
+import Formulario from "@/mixins/Formulario.js"; 
+import "../../assets/css/categoria_store.css";
 export default {
     name: "ProductoDetalle",
     mixins: [Formulario],
