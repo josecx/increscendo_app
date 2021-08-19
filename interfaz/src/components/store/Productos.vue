7acf73675b313822632fc87720dc4b644c6209e7
danytujal <danytujal@gmail.com>
2021-08-18 23:10:09 -0600

    style new cart detail

--- interfaz/src/components/store/Productos.vue
+++ interfaz/src/components/store/Productos.vue
@@ -39,41 +39,7 @@
             <a href="javascript:;" @click="detalleProducto(i)">
                 <button class="buy-1 btn_cards_categoria">Comprar</button>
             </a>
-        </div>
-        <!--  -->
-        <div class="content_categoria" v-for="(i, key) in lista" :key="key">
-            <img class="product_img_categoria" :src="i.imagen_link">
-            <h3 class="categoria_h3">{{i.nombre}}</h3>
-            <p class="p_store_categoria">Descripción del producto</p>
-            <h6 class="price_categoria">Q {{i.precio_venta}}</h6>
-            <ul class="stars_categoria">
-            <li><i class="fa fa-star" aria-hidden="true"></i></li>
-            <li><i class="fa fa-star" aria-hidden="true"></i></li>
-            <li><i class="fa fa-star" aria-hidden="true"></i></li>
-            <li><i class="fa fa-star" aria-hidden="true"></i></li>
-            <li><i class="fa fa-star" aria-hidden="true"></i></li>
-            </ul>
-            <a href="javascript:;" @click="detalleProducto(i)">
-                <button class="buy-1 btn_cards_categoria">Comprar</button>
-            </a>
-        </div>
-        <!--  -->
-        <div class="content_categoria" v-for="(i, key) in lista" :key="key">
-            <img class="product_img_categoria" :src="i.imagen_link">
-            <h3 class="categoria_h3">{{i.nombre}}</h3>
-            <p class="p_store_categoria">Descripción del producto</p>
-            <h6 class="price_categoria">Q {{i.precio_venta}}</h6>
-            <ul class="stars_categoria">
-            <li><i class="fa fa-star" aria-hidden="true"></i></li>
-            <li><i class="fa fa-star" aria-hidden="true"></i></li>
-            <li><i class="fa fa-star" aria-hidden="true"></i></li>
-            <li><i class="fa fa-star" aria-hidden="true"></i></li>
-            <li><i class="fa fa-star" aria-hidden="true"></i></li>
-            </ul>
-            <a href="javascript:;" @click="detalleProducto(i)">
-                <button class="buy-1 btn_cards_categoria">Comprar</button>
-            </a>
-        </div>
+        </div>        
     </div>
 
 
@@ -81,8 +47,78 @@
 
 
         <!-- Detalle del producto -->
-        <div class="card" v-if="actual == 2">
-            <div class="card-header">
+        <div style="border:none;" class="card" v-if="actual == 2">
+            
+            <div class="cart-store-content">
+                <div class="header-cart-detail">
+                    <div>
+                        <h2>{{producto.nombre}}</h2>
+                        <p>SKU: 132154U</p>
+                    </div>
+                    <button class="btn" @click="actual = 1"><i class="fas fa-arrow-left"></i></button>
+                </div>
+                <div class="row-midlecart">
+                    <div class="cart-img-content">
+                        <div class="cart-img-box">
+                            <div class="imag-product-box imag-product-box-active">
+                                <img :src="producto.imagen_link">
+                            </div>
+                            <div class="imag-product-box">
+                                <img :src="producto.imagen_link">
+                            </div>
+                            <div class="imag-product-box">
+                                <img :src="producto.imagen_link">
+                            </div>
+                            <div class="imag-product-box">
+                                <img :src="producto.imagen_link">
+                            </div>
+                            <div class="imag-product-box">
+                                <img :src="producto.imagen_link">
+                            </div>
+                            <div class="imag-product-box">
+                                <img :src="producto.imagen_link">
+                            </div>
+                        </div>
+                        <div class="cart-img-box-principal">
+                            <img :src="producto.imagen_link">
+                        </div>
+                    </div>
+                    <div class="cart-detail">
+                        <div class="cart-price">
+                            <h3>Q150,000.00</h3>
+                            <p class="disponible-text"><i class="fas fa-check-circle"></i> Disponible</p>
+                            <p class="existencias-text"><i class="fas fa-boxes"></i> Solo quedan 4</p>
+                        </div>
+                        <div class="cart-count-detail">
+                            <p class="title-count-cart">Cantidad:</p>
+                            <div class="count-inpt">
+                                <button id='disminuir'>-</button>
+                                <input class="m-d-ipt" type='text' value='1'>
+                                <button id='aumentar'>+</button>
+                            </div>
+                            <button class="add-cart-btn"><i class="fas fa-shopping-cart"></i> Agregar al carrito</button>   
+                        </div>
+                        <div class="cart-other-detail">
+                            <button><i class="fas fa-heart"></i></button>
+                        </div>
+                    </div>
+                </div>
+                <!-- <div class="cart-especific-detail">
+                    <table>
+                        <th>
+                            <td>1</td>
+                            <td>1</td>
+                            <td>1</td>
+                            <td>2</td>
+                        </th>
+                    </table>
+                </div> -->
+            </div>
+
+
+
+
+            <!-- <div class="card-header">
                 <div class="col-12 form-row">
                     <div class="col-11">
                         <h2>{{producto.nombre}}</h2>
@@ -94,9 +130,11 @@
             </div>
             <div class="card-body">
                 <img :src="producto.imagen_link" class="img-thumbnail avatar m-0 p-0">
-            </div>
+            </div> -->
         </div>
         <!-- fin -->
+
+
     </div>
 </template>
 <script>
